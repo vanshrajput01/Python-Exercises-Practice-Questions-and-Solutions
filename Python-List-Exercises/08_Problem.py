3efc43b3a77734580690730183c295a84364285a
vanshrajput01 <rajputvansh009@gmail.com>
2024-02-25 15:39:56 +0530

    another way to solve Problem 08

--- Python-List-Exercises/08_Problem.py
+++ Python-List-Exercises/08_Problem.py
@@ -9,3 +9,20 @@ temp_list.reverse()
 print(temp_list)
 
 # Solution two
+
+temp_list = [1,2,3,4,5,6,7,8]
+
+reverse_list = []
+i  = 0
+
+for element in range(1,len(temp_list)+1):
+    if element == 9:
+        break
+    else:
+        reverse_list.append(temp_list[-element])
+print(reverse_list)
+
+
+
+
+
