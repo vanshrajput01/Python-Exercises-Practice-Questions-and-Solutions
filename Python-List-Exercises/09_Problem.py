916e03729c7eeb99c1de5deacbef8f1716fb4fde
vanshrajput01 <rajputvansh009@gmail.com>
2024-02-25 15:44:44 +0530

    09 solution "Final commit"

--- Python-List-Exercises/09_Problem.py
+++ Python-List-Exercises/09_Problem.py
@@ -1,2 +1,27 @@
 # Python | Cloning or Copying a list
 
+# Solution One
+
+temp_list = ["Mango","Banana","Apple","Green Apple"]
+
+temp_list_Copy = temp_list.copy()
+
+print(temp_list_Copy)
+
+# Solution Two
+
+temp_list = ["Mango","Banana","Apple","Green Apple"]
+
+temp_list_Clone = []
+
+for element in temp_list:
+
+    temp_list_Clone.append(element)
+
+print(temp_list_Clone)
+
+
+
+
+
+
