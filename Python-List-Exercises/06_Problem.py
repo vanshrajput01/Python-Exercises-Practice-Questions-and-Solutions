741bda259f40bca96634e638d5497584ba5bebe2
vanshrajput01 <rajputvansh009@gmail.com>
2024-02-25 14:36:54 +0530

    06 Problem Solution "Final commit"

--- Python-List-Exercises/06_Problem.py
+++ Python-List-Exercises/06_Problem.py
@@ -1,1 +1,12 @@
-#
+# Python | Ways to check if element exists in list
+
+chai_types = ["Masala Chai","Lemon Tea","Ginger Tea","Black Tea","Simple Chai"]
+
+element = input("Enter any Element you find in this list : ")
+
+if chai_types.count(element) == 0:
+    print(f"No. {element} is not in this List")
+
+else:
+    print(f"Yes. {element} is in this List")
+
