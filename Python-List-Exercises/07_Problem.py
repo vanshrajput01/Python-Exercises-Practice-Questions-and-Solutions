2b8758c35bd202096b996715c9fad51467c4403e
vanshrajput01 <rajputvansh009@gmail.com>
2024-02-25 14:54:06 +0530

    07_Problem solution "Final commit"

--- Python-List-Exercises/07_Problem.py
+++ Python-List-Exercises/07_Problem.py
@@ -10,6 +10,26 @@ temp_list.clear()
 
 print(temp_list)
 
+# Solution Two
+
+temp_list = [1,2,3,4,5,6,7,8]
+
+i = 0
+
+
+for element in range(0,len(temp_list)):
+    if element == 8:
+        break
+    else:
+        temp_list.pop()
+
+
+print(temp_list)
+
+
+
+
+
 
 
 
