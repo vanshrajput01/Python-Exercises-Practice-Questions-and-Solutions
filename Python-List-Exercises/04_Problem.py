bd3f527fd1cc6320ad3f33cef72e16574a199fd5
vanshrajput01 <rajputvansh009@gmail.com>
2024-02-25 12:52:51 +0530

    04 solution "Final commit"

---
+++ Python-List-Exercises/04_Problem.py
@@ -0,0 +1,23 @@
+# Maximum of two numbers in Python
+
+# Solution one : -
+
+number_one = int(input("Enter a number : "))
+number_two = int(input("Enter another number : "))
+
+
+
+
+if number_one > number_two:
+    print(f"{number_one}")
+
+else:
+    print(f"{number_two}")
+
+
+# Solution Two : 
+    
+print(max(number_one,number_two))
+    
+
+
