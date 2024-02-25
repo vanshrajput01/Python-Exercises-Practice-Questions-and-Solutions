738b230b7cb574db20f48523b97d6694c5256267
vanshrajput01 <rajputvansh009@gmail.com>
2024-02-25 12:39:31 +0530

    03_Problem "Final commit"

--- Python-List-Exercises/03_Problem.py
+++ Python-List-Exercises/03_Problem.py
@@ -1,1 +1,16 @@
 # Python | Ways to find length of list
+
+# Solution One : -
+
+temp_list = [2,3,4,5,6,7,8,9]
+len_counter = 0
+
+for element in temp_list:
+    len_counter +=1
+
+print(len_counter)
+
+# Solution Two : -
+
+print(len(temp_list))
+
