38d1dcbc5576e42e62de29ca83cf4bf507d885b5
vanshrajput01 <rajputvansh009@gmail.com>
2024-02-25 12:35:06 +0530

    02_Problem "Final commit"

--- Python-List-Exercises/02_Problem.py
+++ Python-List-Exercises/02_Problem.py
@@ -1,6 +1,15 @@
+# Python program to swap two elements in a list
 my_list = [1,2,3,5,6,7]
 
 i = 0
 
 while i < len(my_list):
-    if i == 
+    if i == 2:
+        n = my_list[i]
+        my_list[i] = my_list[len(my_list) - 3]
+        my_list[len(my_list) - 3] = n
+    
+    i +=1
+
+print(my_list)
+    
